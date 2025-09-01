6d41d33b96b5fb14f85b3a15dba2f5b90edb50d1
narvdeshwar <sam.07soni@gmail.com>
2025-09-01 23:07:57 +0530

    menu controller created

--- src/controllers/menu.controller.ts
+++ src/controllers/menu.controller.ts
@@ -1,12 +1,23 @@
 import { Controller } from "../types/expressRouteHandlerTypes";
 import { successResponse } from "../utils/response";
 import * as menuServices from "../services/menu.services"
+import { isValidObjectId } from "mongoose";
+import { ApiError } from "../utils/ApiError";
+import { validEntity } from "../utils/validEntity";
 
 export const createMenu: Controller = async (req, res, next) => {
     try {
-        const menu = await menuServices.createMenu();
+        const RestaurantId = req.params.id;
+        if (!isValidObjectId(RestaurantId))
+            throw new ApiError(404, "Invalid object Id")
+        validEntity(RestaurantId, "Restaurant id");
+        const { title, items } = req.body;
+        validEntity(title, "Title")
+        if (items.length === 0 && !Array.isArray(items))
+            throw new ApiError(422, "Menu items must be non empty!")
+        const menu = await menuServices.createMenu(title, items, RestaurantId);
         return successResponse(res, "Menu created successfully", menu, 200)
     } catch (error) {
         next(error)
     }
-}
+} 
--- src/services/menu.services.ts
+++ src/services/menu.services.ts
@@ -1,3 +1,10 @@
-export const createMenu = async () => {
+interface itemsProps {
+    name: String,
+    price: Number,
+    description?: String,
+    isAvailable?: Boolean
+}[]
+export const createMenu = async (title: string, items: itemsProps[], RestaurantId: string) => {
+
     return createMenu;
 }
