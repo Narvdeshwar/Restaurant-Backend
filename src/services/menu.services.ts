18a9d06b50cdcfbcb4fbeba601aa77edd73717cb
narvdeshwar <sam.07soni@gmail.com>
2025-09-02 23:17:31 +0530

    feat: added the service of menu

--- src/services/menu.services.ts
+++ src/services/menu.services.ts
@@ -1,3 +1,6 @@
+import Menu from "../models/menu.models";
+import { ApiError } from "../utils/ApiError";
+
 interface itemsProps {
     name: String,
     price: Number,
@@ -5,6 +8,16 @@ interface itemsProps {
     isAvailable?: Boolean
 }[]
 export const createMenu = async (title: string, items: itemsProps[], RestaurantId: string) => {
-
-    return createMenu;
+    const menu = await Menu.findOneAndUpdate({ _id: RestaurantId, title },
+        {
+            $setOnInsert: { title, RestaurantId },
+            $addToSet: { items: { $each: items } }
+        },
+        {
+            new: true,
+            upsert: true
+        }
+    );
+    if (!menu) throw new ApiError(500, "Unable to add/update the menu");
+    return menu;
 }
