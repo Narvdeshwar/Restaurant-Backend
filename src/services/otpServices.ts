e7016238880f04dfc616d67c9c61e5d266c99a2d
narvdeshwar <sam.07soni@gmail.com>
2025-09-10 23:50:37 +0530

    fix: remove the key from redis after verify the otp

--- src/services/otpServices.ts
+++ src/services/otpServices.ts
@@ -14,12 +14,16 @@ export const storeOTP = () => {
 }
 
 export const verifyOTP = async (otp: string, userId: string) => {
-    const key = `otp:${userId}`
     if (!userId) throw new ApiError(400, "User id is required.")
     if (!otp) throw new ApiError(400, "OTP required for email verification")
+
+    const key = `otp:${userId}`
     const hashedOTP = await redis.get(key)
     if (!hashedOTP) throw new ApiError(404, "Either OTP is expired or time exceed")
+
     const isOtpMatched = await bcrypt.compare(otp, hashedOTP)
     if (!isOtpMatched) throw new ApiError(422, "OTP is invalid")
+
+    await redis.del(key)
     return true;
 }
